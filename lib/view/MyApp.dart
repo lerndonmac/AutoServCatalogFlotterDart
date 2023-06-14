8f0368dea19a455b518d22cc7a79dd86df5e0b6a
Donmas <fadeevdimas2001@gmail.com>
2023-06-15 02:05:14 +0300

    add comments

--- lib/view/MyApp.dart
+++ lib/view/MyApp.dart
@@ -17,33 +17,50 @@ class HomeState extends State<MyApp>{
   List carsList = [];
 //метод запускаемый перед отрисовкой экрана
   @override
-  void initState() {
+  void initState(){
     super.initState();
+    jsonRead();
   }
+  //метод чтения данных из json
   void jsonRead() async{
+    //json файл читается как строка
     final String response = await rootBundle.loadString("assets/json_test.json");
+    //конвертация из строки в результативный объект json
     final data = await json.decode(response);
     setState(() {
+      //заполнение листа по ключу employees
       carsList = data["employees"];
     });
 
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //задаётся цвет фона
       backgroundColor: Colors.white,
+          //построение верхней панели экрана
           appBar: AppBar(
+            //текст в верху окна
             title: const Text("Список Авто"),
+            //размещение текста по центру
             centerTitle: true,
           ),
+          //построение основной части экрана
+        //проверяется что массив не пуст после чего создаётся виджет ListView через Builder
           body: carsList.isNotEmpty?ListView.builder(
+            //задаётся количество элементов на осноле длинны массива carsList
             itemCount: carsList.length,
+            //задаётся itemBuilder
             itemBuilder: (context, index){
+              //лист заполняется карточками для отображения данных
               return Card(
+                //задаётся ключ элемента листа
                 key: ValueKey(carsList[index]["name"]),
+                //выставляется отступ между картами
                 margin: const EdgeInsets.all(10),
-                color: Colors.blue,
+                //задаётся цвет фона карточек
+                color: const Color.fromARGB(100, 119, 221, 246),
+                //прописываются параметры карточек
                 child: ListTile(
                   leading: Text(carsList[index]["name"]),
                   title: Text(carsList[index]["type"]),
@@ -52,9 +69,9 @@ class HomeState extends State<MyApp>{
                 ),
               );
             },
+            //кнопка
           ): ElevatedButton(
             onPressed: (){
-              jsonRead();
             },
             child: const Center(child: Text("load json"),
             ),
